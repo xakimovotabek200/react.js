b9e4deac157a99fb464b5680ac53bec79480b6a4
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-02-14 17:06:28 +0300

    cardlar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Card from "./cards/Card";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+      <Card/>
     </div>
   );
 }
---
+++ src/cards/Card.jsx
@@ -0,0 +1,14 @@
+import "./kard.css";
+function Card(){
+    return <>
+    <div className="card">
+      <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    </div>
+    </>
+}
+export default Card;
