05ba9effb26b6ca6e95746e0be61bc163abab394
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-02-14 17:37:51 +0300

    imgli

--- src/cards/Card.jsx
+++ src/cards/Card.jsx
@@ -1,14 +1,19 @@
 import "./kard.css";
-function Card(){
-    return <>
-    <div className="card">
-      <div className="box"></div>
-    <div className="box"></div>
-    <div className="box"></div>
-    <div className="box"></div>
-    <div className="box"></div>
-    <div className="box"></div>
-    </div>
+function Card() {
+  return (
+    <>
+      <div className="card">
+        <div className="box">
+          <img src="./img/1.png" alt="" />
+        </div>
+        <div className="box">
+        </div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
     </>
+  );
 }
 export default Card;
